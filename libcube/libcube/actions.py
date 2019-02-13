2be54f107d915d1557ac344fbbb7ef69421a402f
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-02-14 00:09:26 +0300

    Turning action.

--- libcube/libcube/actions.py
+++ libcube/libcube/actions.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Union, List
 
 from .cube import Cube
 from .orientation import Orientation, Side
@@ -12,7 +13,7 @@ class Action(ABC):
 
 class Rotate(Action):
     def __init__(self, around: Side):
-        self.axis_side = around
+        self.axis_side: Side = around
 
     def perform(self, cube: Cube, orientation: Orientation) -> Orientation:
         if self.axis_side == Side.FRONT:
@@ -27,3 +28,26 @@ class Rotate(Action):
             return orientation.to_right
         else:
             return orientation.to_left
+
+
+class Turn(Action):
+    def __init__(self, side: Side, sides: Union[int, List[int]], turns: int = 1):
+        self.side: Side = side
+        self.sides: List[int] = sides if isinstance(sides, list) else [sides]
+        if side in {Side.BOTTOM, Side.RIGHT, Side.FRONT}:
+            self.turns: int = turns
+        else:
+            self.turns: int = 4 - turns
+        self.index_multiplier = 1 if side not in {Side.BACK, Side.RIGHT, Side.BOTTOM} else -1
+
+    def perform(self, cube: Cube, orientation: Orientation) -> Orientation:
+        if self.side == Side.LEFT or self.side == Side.RIGHT:
+            rotate_function = cube.rotate_vertical
+        elif self.side == Side.TOP or self.side == Side.BOTTOM:
+            rotate_function = cube.rotate_horizontal
+        else:
+            rotate_function = cube.rotate_slice
+
+        for side in self.sides:
+            rotate_function(orientation, side * self.index_multiplier, self.turns)
+        return orientation
--- libcube/test/test_actions.py
+++ libcube/test/test_actions.py
@@ -1,7 +1,10 @@
+from libcube.cube import Cube
 from libcube.orientation import Side, Orientation
-from libcube.actions import Rotate
+from libcube.actions import Rotate, Turn
 
 import pytest
+from typing import List
+from unittest.mock import MagicMock
 
 
 @pytest.mark.parametrize("around, front, top", [
@@ -12,7 +15,7 @@ import pytest
     (Side.TOP, Side.RIGHT, Side.TOP),
     (Side.BOTTOM, Side.LEFT, Side.TOP)
 ])
-def test_rotations(around: Side, front: Side, top: Side):
+def test_rotations(around: Side, front: Side, top: Side) -> None:
     orientation = Orientation(Side.FRONT, Side.TOP)
     action = Rotate(around)
     # noinspection PyTypeChecker
@@ -20,3 +23,32 @@ def test_rotations(around: Side, front: Side, top: Side):
 
     assert orientation.front == front
     assert orientation.top == top
+
+
+class Object(object):
+    pass
+
+
+@pytest.mark.parametrize("side, func, out_sides, out_amount", [
+    (Side.FRONT, "rotate_slice", [1, 2], 1),
+    (Side.BACK, "rotate_slice", [-1, -2], 3),
+    (Side.RIGHT, "rotate_vertical", [-1, -2], 1),
+    (Side.LEFT, "rotate_vertical", [1, 2], 3),
+    (Side.TOP, "rotate_horizontal", [1, 2], 3),
+    (Side.BOTTOM, "rotate_horizontal", [-1, -2], 1)
+])
+def test_turning_vertical(side: Side, func: str, out_sides: List[int], out_amount: int) -> None:
+    cube: Cube = Object()
+    mock = MagicMock()
+    setattr(cube, func, mock)
+
+    orientation = Orientation()
+    action = Turn(side, [1, 2], 1)
+    assert action.perform(cube, orientation) == orientation
+    assert len(mock.call_args_list) == len(out_sides)
+    print(mock.call_args_list[0])
+    for args, side in zip(mock.call_args_list, out_sides):
+        arg_orientation, arg_index, arg_turn = tuple(args)[0]
+        assert arg_orientation == orientation
+        assert arg_index == side
+        assert arg_turn == out_amount
