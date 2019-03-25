ccb4489f73b8910920ebdfa413c6140315adf0c9
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-03-25 23:23:38 +0300

    Switched to using alt. method of handlign tree nodes

--- libcube/compiler/compiler.py
+++ libcube/compiler/compiler.py
@@ -1,6 +1,7 @@
-from lark import Lark, Transformer
-from typing import IO, Union, NamedTuple, Tuple, List
 from pathlib import Path
+from typing import IO, Union, NamedTuple, Tuple, List, Callable, Dict
+
+from lark import Lark, Tree
 
 from .expression import Expression, TemplateType
 from .types import Integer, Real, Type
@@ -12,39 +13,7 @@ class BinaryOperator(NamedTuple):
     arguments: List[Tuple[Tuple[Type, Type], Type]]
 
 
-# noinspection PyMethodMayBeStatic
-class CompilerTransformer(Transformer):
-    BINARY_OPERATORS = [[
-        BinaryOperator("+", ["(", 0, ") + (", 1, ")"], [((Integer, Integer), Integer), ((Real, Real), Real)]),
-        BinaryOperator("-", ["(", 0, ") - (", 1, ")"], [((Integer, Integer), Integer), ((Real, Real), Real)]),
-        BinaryOperator("*", ["(", 0, ") * (", 1, ")"], [((Integer, Integer), Integer), ((Real, Real), Real)]),
-        BinaryOperator("/", ["(", 0, ") / (", 1, ")"], [((Real, Real), Real)]),
-    ]]
-
-    @staticmethod
-    def operator_method(operator: BinaryOperator, args: List[Expression]):
-        assert len(args) == 2
-        arg_types = [x.type for x in args]
-        for operand_types, result_type in operator.arguments:
-            if all(ar1.is_assignable(ar2) for ar1, ar2 in zip(operand_types, arg_types)):
-                return Expression.merge(result_type, operator.expression, *args)
-        else:
-            raise ValueError(f"Operator '{operator.symbol}' is not applicable to values "
-                             f"of type {arg_types[0]} and {arg_types[1]}")
-
-    def int_literal(self, values):
-        value = int(values[0])
-        return Expression(Integer, str(value))
-
-    def float_literal(self, values):
-        value = float(values[0])
-        return Expression(Real, str(value))
-
-
-for precedence, operators in enumerate(CompilerTransformer.BINARY_OPERATORS):
-    for i, operator in enumerate(operators):
-        setattr(CompilerTransformer, f"op_{precedence}_{i}",
-                lambda self, args, op=operator: CompilerTransformer.operator_method(op, args))
+Callback = Callable[[Tree], Union[Type, Expression]]
 
 
 class Compiler:
@@ -53,9 +22,53 @@ class Compiler:
         with open(str(path)) as f:
             grammar = f.read()
         self.lark = Lark(grammar)
+        self.callbacks: Dict[str, Callback] = dict()
 
     def compile(self, file: Union[str, IO[str]]):
         if not isinstance(file, str):
             file = file.read()
         tree = self.lark.parse(file)
-        return CompilerTransformer().transform(tree)
+        pass
+
+    def handler(self, name: str) -> Callable[[Callback], Callback]:
+        def wrapper(func: Callback) -> Callback:
+            self.callbacks[name] = func
+            return func
+        return wrapper
+
+    def handle(self, tree: Tree) -> Union[Expression, Type]:
+        return self.callbacks[tree.data](tree)
+
+
+compiler = Compiler()
+BINARY_OPERATORS = [[
+    BinaryOperator("+", ["(", 0, ") + (", 1, ")"], [((Integer, Integer), Integer), ((Real, Real), Real)]),
+    BinaryOperator("-", ["(", 0, ") - (", 1, ")"], [((Integer, Integer), Integer), ((Real, Real), Real)]),
+    BinaryOperator("*", ["(", 0, ") * (", 1, ")"], [((Integer, Integer), Integer), ((Real, Real), Real)]),
+    BinaryOperator("/", ["(", 0, ") / (", 1, ")"], [((Real, Real), Real)]),
+]]
+
+for precedence, operators in enumerate(BINARY_OPERATORS):
+    for i, operator in enumerate(operators):
+        @compiler.handler(f"op_{precedence}_{i}")
+        def handler(tree: Tree, op=operator) -> Expression:
+            assert len(tree.children) == 2
+            expr1: Expression = compiler.handle(tree.children[0])
+            expr2: Expression = compiler.handle(tree.children[1])
+            arg_types = [expr1.type, expr2.type]
+            for operand_types, result_type in op.arguments:
+                if all(ar1.is_assignable(ar2) for ar1, ar2 in zip(operand_types, arg_types)):
+                    return Expression.merge(result_type, op.expression, expr1, expr2)
+            else:
+                raise ValueError(f"Operator '{op.symbol}' is not applicable to values "
+                                 f"of type {arg_types[0]} and {arg_types[1]}")
+
+
+@compiler.handler("int_literal")
+def handle_int_literal(tree: Tree) -> Expression:
+    return Expression(Integer, str(int(tree.children[0])))
+
+
+@compiler.handler("float_literal")
+def handle_float_literal(tree: Tree) -> Expression:
+    return Expression(Real, str(float(tree.children[0])))
--- test/libcube/compiler/test_compiler.py
+++ test/libcube/compiler/test_compiler.py
@@ -1,42 +1,36 @@
 import lark
 import pytest
 
-from libcube.compiler.compiler import CompilerTransformer
+from libcube.compiler.compiler import compiler
 from libcube.compiler.expression import Expression
 from libcube.compiler.types import Integer, Real, Type, Bool
 
 
 # noinspection PyMethodMayBeStatic
 class TestTransformer:
-    @pytest.fixture()
-    def transform(self) -> CompilerTransformer:
-        return CompilerTransformer()
 
     @pytest.mark.parametrize("name, value, exp_type, exp_value", [
         ("int_literal", "1", Integer, "1"),
         ("float_literal", "1.4", Real, "1.4")
     ])
-    def test_literal(self, name: str, value: str, exp_type: Type, exp_value: str, transform: CompilerTransformer):
+    def test_literal(self, name: str, value: str, exp_type: Type, exp_value: str):
         tree = lark.Tree(name, [value])
-        expr = transform.transform(tree)
+        expr = compiler.handle(tree)
         assert isinstance(expr, Expression)
         assert expr.type == exp_type
         assert expr.expression == [exp_value]
         assert expr.intermediates == []
 
     @pytest.mark.parametrize("op1_type, op2_type, op_name, res_type, res_expr", [
-        (Integer, Integer, "op_0_0", Integer, "(a) + (b)"),
-        (Real, Integer, "op_0_0", Real, "(a) + (b)"),
-        (Integer, Real, "op_0_0", Real, "(a) + (b)"),
-        (Integer, Integer, "op_0_3", Real, "(a) / (b)")
+        ("int_literal", "int_literal", "op_0_0", Integer, "(1) + (2)"),
+        ("float_literal", "int_literal", "op_0_0", Real, "(1.0) + (2)"),
+        ("int_literal", "float_literal", "op_0_0", Real, "(1) + (2.0)"),
+        ("int_literal", "int_literal", "op_0_3", Real, "(1) / (2)")
     ])
-    def test_operators(self, op1_type: Type, op2_type: Type, op_name: str, res_type: Type,
-                       res_expr: str, transform: CompilerTransformer):
-        a_expr = Expression(op1_type, ["a"])
-        b_expr = Expression(op2_type, ["b"])
-        tree = lark.Tree(op_name, [a_expr, b_expr])
+    def test_operators(self, op1_type: str, op2_type: str, op_name: str, res_type: Type, res_expr: str):
+        tree = lark.Tree(op_name, [lark.Tree(op1_type, ["1"]), lark.Tree(op2_type, ["2"])])
 
-        expr: Expression = transform.transform(tree)
+        expr: Expression = compiler.handle(tree)
         assert expr.type == res_type
         assert expr.intermediates == []
         assert "".join(expr.expression) == res_expr
@@ -44,9 +38,9 @@ class TestTransformer:
     @pytest.mark.parametrize("op1_type, op2_type, op_name", [
         (Bool, Integer, "op_0_0"),
     ])
-    def test_operators_invalid(self, op1_type: Type, op2_type: Type, op_name: str, transform: CompilerTransformer):
+    def test_operators_invalid(self, op1_type: Type, op2_type: Type, op_name: str):
         a_expr = Expression(op1_type, ["a"])
         b_expr = Expression(op2_type, ["b"])
         tree = lark.Tree(op_name, [a_expr, b_expr])
         with pytest.raises(Exception):
-            transform.transform(tree)
+            compiler.handle(tree)
