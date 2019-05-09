e3a752c3ca0d91bfdc449d936c87242c6041065d
Vlad Poletaev <poletaev.vladislav@gmail.com>
2019-05-09 23:51:56 +0300

    Writing code for the error displaying utility

--- libcube/cli/error_display.py
+++ libcube/cli/error_display.py
@@ -1,25 +1,68 @@
 from click import secho
+from typing import IO, Iterable, Tuple
 
 
-class DisplayOptions:
-    pass
+class ErrorsOutput:
+    def __init__(self, stream: IO):
+        self.stream = stream
+        self.max_width = 80
+        self.use_color = True
+        self.line_number_margin = 2
 
+    def _echo(self, text: str, color: str = None, nl: bool = False) -> None:
+        if self.use_color and color is not None:
+            self.stream.write(text)
+            if nl:
+                self.stream.write("\n")
+        else:
+            secho(text, self.stream, nl, color=color)
 
-def display_code_line(file, text: str, number: int, number_length: int,
-                      max_width: int, use_color: bool = True):
-    fopt = dict(color=use_color, reset=use_color)
+    def _write_lines(self, line_num: str, text: str) -> \
+            Iterable[Tuple[int, int]]:
+        offset = 0
+        width = self.max_width - self.line_number_margin - len(line_num)
+        while offset < len(text):
+            if offset == 0:
+                self._echo(line_num + " " * self.line_number_margin, "cyan")
+            else:
+                self._echo(" " * (len(line_num) + self.line_number_margin))
+            substring = text[offset: offset + width]
+            self._echo(substring, nl=True)
+            yield offset, offset + len(substring)
+            offset += len(substring)
 
-    max_width -= number_length + 2
-    secho(f"{number:{number_length}}  ", file, nl=False, **fopt)
-    offset = 0
-    while offset < len(text):
-        secho(text[offset: offset + max_width], file, **fopt)
-        offset += max_width
-        if offset < len(text):
-            secho(" " * (number_length + 2), file, nl=False, **fopt)
+    def _write_lines_with_error(self, line_num: str, text: str,
+                                start_col: int, end_col: int) -> None:
+        for first, last in self._write_lines(line_num, text):
+            if start_col >= last or end_col < first:
+                continue
+            error_start = max(first, start_col) - first
+            error_end = min(last, end_col + 1) - first
+            self._echo(" " * (error_start + self.line_number_margin + len(line_num)))
+            self._echo("~" * (error_end - error_start), nl=True)
 
+    def _write_lines_all(self, line_num: str, text: str) -> None:
+        for _, _ in self._write_lines(line_num, text):
+            continue
 
-def display_code_error(file, text: str, start_line: int, end_line: int,
-                       start_column: int = -1, end_column: int = -1,
-                       use_color: bool = True):
-    pass
+    def display_code(self, code: str, start_line: int, start_column: int,
+                     end_line: int, end_column: int) -> None:
+        line_num_width = len(str(end_line))
+        code = code.split("\n")
+
+        def write(ln: int) -> None:
+            self._write_lines_all(f"{ln + 1:{line_num_width}}", code[ln])
+
+        if end_line - start_line >= 5:
+            for line in range(start_line, start_line + 2):
+                write(line)
+            self._echo("...", nl=True)
+            for line in range(end_line - 1, end_line + 1):
+                write(line)
+        elif end_line != start_line:
+            for line in range(start_line, end_line + 1):
+                write(line)
+        else:
+            line_num = f"{start_line + 1:{line_num_width}}"
+            self._write_lines_with_error(line_num, code[start_line],
+                                         start_column, end_column)
--- test/libcube/cli/test_error_display.py
+++ test/libcube/cli/test_error_display.py
@@ -1,19 +1,38 @@
 from io import StringIO
 
-from libcube.cli.error_display import display_code_line
+from libcube.cli.error_display import ErrorsOutput
 
 
-def test_display_error_line():
+def test_code_single_line():
     file = StringIO()
-    text = "01234567890123456789012345"
-    display_code_line(file, text, 13, 3, 15, use_color=False)
+    errors = ErrorsOutput(file)
+    errors.max_width = 11
+    errors.line_number_margin = 1
+
+    text = "0123456789" * 3
+    errors.display_code(text, 0, 10, 0, 20)
+
     result = file.getvalue()
-    assert " 13  0123456789\n     0123456789\n     012345\n" == result
+    assert "1 012345678\n  901234567\n   ~~~~~~~~\n  890123456\n  ~~~\n  789\n" == result
 
 
-def test_display_error_single_line():
+def test_code_many_lines():
     file = StringIO()
-    text = "0123"
-    display_code_line(file, text, 10, 3, 15, use_color=False)
+    errors = ErrorsOutput(file)
+
+    text = "\n".join("abcdefghi")
+    errors.display_code(text, 2, 0, 7, 0)
+
     result = file.getvalue()
-    assert " 10  0123\n" == result
+    assert "3  c\n4  d\n...\n7  g\n8  h\n" == result
+
+
+def test_code_few_lines():
+    file = StringIO()
+    errors = ErrorsOutput(file)
+
+    text = "\n".join("abcdefghijklmnopq")
+    errors.display_code(text, 8, 0, 11, 0)
+
+    result = file.getvalue()
+    assert " 9  i\n10  j\n11  k\n12  l\n" == result
